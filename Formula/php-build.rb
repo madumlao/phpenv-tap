2dfe2bdfbbb7d334d2d568c9ebafdcd444bf652b
madumlao <madumlao@gmail.com>
2018-10-14 12:07:17 +0800

    Update php-build

--- Formula/php-build.rb
+++ Formula/php-build.rb
@@ -2,7 +2,7 @@ class PhpBuild < Formula
   desc "Builds PHP so that multiple versions can be used side by side"
   homepage "https://github.com/php-build/php-build"
   url "https://github.com/madumlao/php-build/archive/osx-zlib.zip"
-  sha256 "d9f1452d345916e51db937c84dafaa89c66feb0660261935c18e502575f2205e"
+  sha256 "310f0a25252870fc4d1516f71c630f5ce5b3647687a2d0355c6fb49174fd163b"
   head "https://github.com/madumlao/php-build.git"
 
   bottle :unneeded

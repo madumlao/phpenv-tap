5ebe329d094e0725120a39075570e303097e6e63
madumlao <madumlao@gmail.com>
2018-10-14 13:29:30 +0800

    Added phpenv formula

---
+++ Formula/phpenv.rb
@@ -0,0 +1,33 @@
+class Phpenv < Formula
+  desc "Simple PHP version management"
+  homepage "https://github.com/phpenv/phpenv#readme"
+  head "https://github.com/phpenv/phpenv.git"
+
+  depends_on "php-build" => :recommended
+
+  def install
+    inreplace "libexec/phpenv" do |s|
+      s.gsub! '"${BASH_SOURCE%/*}"/../libexec', libexec
+      if HOMEBREW_PREFIX.to_s != "/usr/local"
+        s.gsub! ":/usr/local/etc/phpenv.d", ":#{HOMEBREW_PREFIX}/etc/phpenv.d\\0"
+      end
+    end
+
+    # Compile optional bash extension.
+    system "src/configure"
+    system "make", "-C", "src"
+
+    if build.head?
+      # Record exact git revision for `phpenv --version` output
+      git_revision = `git rev-parse --short HEAD`.chomp
+      inreplace "libexec/phpenv---version", /^(version=)"([^"]+)"/,
+                                           %Q(\\1"\\2-g#{git_revision}")
+    end
+
+    prefix.install ["bin", "completions", "libexec", "phpenv.d"]
+  end
+
+  test do
+    shell_output("eval \"$(#{bin}/phpenv init -)\" && phpenv versions")
+  end
+end
